f821bf9c2b1e94110192c9b11db1f59bdfa13d4c
Jes Bak Hansen <jeshansen@gmail.com>
2019-10-30 23:16:24 +0100

    This is terrible, but it kinda works...
    at least until 2021/10/01, so...

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Ticker from "./components/Ticker";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">                
-        <p>HELLO WORLD!</p>        
+      <header className="App-header">
+        <Ticker />
       </header>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Ticker.tsx
@@ -0,0 +1,30 @@
+// Ticker.tsx
+
+import React, { useState, useEffect } from "react";
+import TickLogic from "./TickLogic";
+
+const Ticker: React.FC = () => {
+  const [pctMyHouse, setHousePct] = useState([0, 0]);
+
+  let tl = new TickLogic();
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setHousePct(() => tl.GetMyHousePercentage(new Date()));
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  var formatter = new Intl.NumberFormat("da-DK", {
+    style: "currency",
+    currency: "DKK"
+  });
+
+  return (
+    <div>
+      Vi ejer {pctMyHouse[0].toFixed(9)} % af huset. <br></br>Vi skylder{" "}
+      {formatter.format(pctMyHouse[1])}
+    </div>
+  );
+};
+
+export default Ticker;
