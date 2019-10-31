3889e5f5e0d61e31bbff01518e2e9ef44a2627db
Jes Bak Hansen <jebh@forca.dk>
2019-10-31 12:57:05 +0100

    Updates

--- precache-manifest.e733c6742d40b29d8b429d857d10e6e4.js
+++ precache-manifest.e733c6742d40b29d8b429d857d10e6e4.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5ebfe4304684c8e9d4bf3ba8e34e36b1",
+    "revision": "11480e776a0d2c45d9461f1b9ce04866",
     "url": "/kaerskiftevej63/index.html"
   },
   {
-    "revision": "48e5e0dbcca026ed7aaa",
+    "revision": "f9aa58e66f6c2f26f58c",
     "url": "/kaerskiftevej63/static/css/main.7c421f3c.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/kaerskiftevej63/static/js/2.a7fa63cc.chunk.js"
   },
   {
-    "revision": "48e5e0dbcca026ed7aaa",
-    "url": "/kaerskiftevej63/static/js/main.0ce99e97.chunk.js"
+    "revision": "f9aa58e66f6c2f26f58c",
+    "url": "/kaerskiftevej63/static/js/main.0cb36bfd.chunk.js"
   },
   {
     "revision": "bf1b677dd11ffe003cf3",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kaerskiftevej63/precache-manifest.7679ff51da7cbc7f83024ec528cd9020.js"
+  "/kaerskiftevej63/precache-manifest.e733c6742d40b29d8b429d857d10e6e4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kaerskiftevej63/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
